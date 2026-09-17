c8f3aae5ad8794c5963e29bf0f1fd31ede7d7da3
Toshi <luckyta2002@gmail.com>
2026-09-17 11:31:02 -0400

    added versioning to the app

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
-
+import { VersioningType } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
   app.setGlobalPrefix('api');
+  app.enableVersioning({
+  type: VersioningType.URI,
+  defaultVersion: '1',
+});
 }
 await bootstrap();
