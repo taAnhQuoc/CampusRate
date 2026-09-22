666c4b1b923613b42bdd7eff2d7562fe1210a28f
Toshi <luckyta2002@gmail.com>
2026-09-22 15:11:09 -0400

    added json fonctions to campus rate and also nest/config for it to work as well

--- src/place/place.service.ts
+++ src/place/place.service.ts
@@ -1,53 +1,87 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException, OnModuleInit } from '@nestjs/common';
 import { Place } from './entities/place.entity.js';
 import { CreatePlaceDTO } from './entities/dto/create-place.dto.js';
 import { UpdatePlaceDTO } from './entities/dto/update-place.dto.js';
-@Injectable()
-export class PlaceService {
+import { ConfigService } from '@nestjs/config';
+import { promises as fs } from 'fs';
+import { dirname } from 'path';
 
-    private readonly places: Place[] = [];
+@Injectable()
+export class PlaceService implements OnModuleInit {
+
+    private places: Place[] = [];
+    private readonly dataPath: string;
+
+    constructor(private readonly configService: ConfigService) {
+        this.dataPath = this.configService.get<string>('DATA_PATH', './data/places.json');
+    }
+
+    async onModuleInit(): Promise<void> {
+        this.places = await this.loadFromLocal();
+    }
+
+    private async loadFromLocal(): Promise<Place[]> {
+        try {
+            const raw = await fs.readFile(this.dataPath, 'utf-8');
+            return JSON.parse(raw) as Place[];
+        } catch (err: any) {
+            if (err.code === 'ENOENT') {
+                return [];
+            }
+            throw err;
+        }
+    }
+
+    private async saveToLocal(): Promise<void> {
+        await fs.mkdir(dirname(this.dataPath), { recursive: true });
+        await fs.writeFile(this.dataPath, JSON.stringify(this.places, null, 2), 'utf-8');
+    }
 
     findAll(): Place[] {
         return this.places;
     }
 
     findOne(id: string): Place {
-        const place: Place | undefined = this.places.find((place: Place) => place.id === id )
+        const place = this.places.find((place: Place) => place.id === id);
 
-        if(!place) {
+        if (!place) {
             throw new NotFoundException(`The place Id ${id} doesn't exist`);
         }
 
         return place;
     }
 
-    create(createPlaceDTO: CreatePlaceDTO) {
+    async create(createPlaceDTO: CreatePlaceDTO): Promise<Place> {
+        const newPlace: Place = new Place(createPlaceDTO);
+        Object.assign(newPlace, createPlaceDTO);
 
-        const newPlace: Place = new Place(createPlaceDTO)
+        this.places.push(newPlace);
 
-        Object.assign(newPlace, createPlaceDTO)
-
-        this.places.push();
+        await this.saveToLocal();
 
         return newPlace;
     }
 
-    update(id: string, updatePlaceDTO: UpdatePlaceDTO): Place {
+    async update(id: string, updatePlaceDTO: UpdatePlaceDTO): Promise<Place> {
         const place: Place = this.findOne(id);
 
         Object.assign(place, updatePlaceDTO);
-       place.updatedAt = new Date();
+        place.updatedAt = new Date();
 
-       return place;
+        await this.saveToLocal();
+
+        return place;
     }
 
-    remove(id: string) {
-        const index: number = this.places.findIndex((place: Place) => place.id === id);
+    async remove(id: string): Promise<void> {
+        const index = this.places.findIndex((place: Place) => place.id === id);
 
         if (index === -1) {
-            throw new NotFoundException(`The place Id ${id} doesn't exist`)
+            throw new NotFoundException(`The place Id ${id} doesn't exist`);
         }
 
-        this.places.splice(index,1);
+        this.places.splice(index, 1);
+
+        await this.saveToLocal();
     }
 }
